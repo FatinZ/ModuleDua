7620ecb9d77425dc83a512bb0a612fb65365ec25
unknown <zahidah2000@gmail.com>
2019-11-17 16:27:53 +0800

    third commit

--- src/app/pages/view/view.page.ts
+++ src/app/pages/view/view.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-view',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewPage implements OnInit {
 
-  constructor() { }
+  data = [];
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.route.queryParams.subscribe(params => {
+      if(this.router.getCurrentNavigation().extras.state){
+        this.data = this.router.getCurrentNavigation().extras.state.data;
+        console.log("Data is ", this.data)
+      }
+    })
+   }
 
   ngOnInit() {
   }
-
 }
